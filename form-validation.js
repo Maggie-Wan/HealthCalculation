ace02ed7bc29058f90d977ca1e5cf71297e0003e
muchone1981 <muchone1981@gmail.com>
2021-05-06 13:46:35 +0800

    避免計算結果後刷新動作

--- form-validation.js
+++ form-validation.js
@@ -9,10 +9,10 @@
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
+       // if (!form.checkValidity()) { //因為不送出資料就不刷新畫面，不管有沒有填寫必要欄位都要停在原畫面，使用preventDefault
           event.preventDefault()
           event.stopPropagation()
-        }
+       // }
 
         form.classList.add('was-validated')
       }, false)
